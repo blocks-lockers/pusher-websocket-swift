c01548979d9d0e859f85ccd3710126fbf8c8ffb9
Alex Studnicka <astudnicka@gmail.com>
2021-04-02 14:04:30 +0200

    Add WebSocketKit

--- Package.swift
+++ Package.swift
@@ -9,14 +9,22 @@ let package = Package(
 		.library(name: "PusherSwift", targets: ["PusherSwift"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/blocks-lockers/websocket-kit", .upToNextMajor(from: "2.1.2")),
+		.package(url: "https://github.com/apple/swift-nio.git", from: "2.26.0"),
+		.package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.0.0"),
+//		.package(url: "https://github.com/blocks-lockers/websocket-kit", .upToNextMajor(from: "2.1.2")),
 //		.package(url: "https://github.com/bitmark-inc/tweetnacl-swiftwrap", .upToNextMajor(from: "1.0.0")),
 	],
 	targets: [
 		.target(
 			name: "PusherSwift",
 			dependencies: [
-				"WebSocketKit",
+				.product(name: "NIO", package: "swift-nio"),
+				.product(name: "NIOConcurrencyHelpers", package: "swift-nio"),
+				.product(name: "NIOFoundationCompat", package: "swift-nio"),
+				.product(name: "NIOHTTP1", package: "swift-nio"),
+				.product(name: "NIOSSL", package: "swift-nio-ssl"),
+				.product(name: "NIOWebSocket", package: "swift-nio"),
+//				"WebSocketKit",
 //				"TweetNacl",
             ],
             path: "Sources"
